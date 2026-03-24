c7e9c5647e3f38bd864e5d735184086f3668b375
ExythAI <james@exythai.com>
2026-03-24 01:02:22 -0230

    Add EF migration for MediaSources table

---
+++ src/PhotoViewer.Api/Migrations/20260324033204_AddMediaSources.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhotoViewer.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaSources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MediaSources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Path = table.Column<string>(type: "TEXT", nullable: false),
+                    Label = table.Column<string>(type: "TEXT", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaSources", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$nePbV6x5MQ.P5Ep618M/xOVRo66SSU/hPjV9Ns8TLiRD6bUGgdKBi");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MediaSources");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$GYgaJz03GOZP9z5aHRJeluIV1WpN1in0O34SNsXPdCYKCLbXZ1CB2");
+        }
+    }
+}
